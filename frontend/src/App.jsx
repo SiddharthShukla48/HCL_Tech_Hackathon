4aedc5c3857d2d2d4a8d7fef10d4304609c7a397
Sneha Agarwal <snehaagarwal1322@gmail.com>
2026-08-29 23:32:35 +0530

    style: update heading styling with text centering and background color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-lg font-bold text-blue-600 item-center"> HCL TECH HACKATHON</h1>
+      <h1 className="text-lg font-bold text-blue-600 text-center bg-red-400"> HCL TECH HACKATHON</h1>
+      
     </>
   )
 }
